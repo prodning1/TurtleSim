1714c7ac2962268e801cdf36c31ee990fd67afa8
Philip Rodning <philip.rodning@gmail.com>
2013-11-08 15:05:40 -0500

    Fixed imports, added get/setters for combatGroup in preparation for ACS, commented out verbosity stuff

--- TurtleSimKernel/src/com/prodning/turtlesim/combat/Fleet.java
+++ TurtleSimKernel/src/com/prodning/turtlesim/combat/Fleet.java
@@ -8,9 +8,9 @@ import java.util.Random;
 
 import com.prodning.turtlesim.combat.CombatEntity.CombatEntityType;
 import com.prodning.turtlesim.combat.data.MacroCombatResult;
+import com.prodning.turtlesim.combat.data.MacroCombatResult.ResultType;
 import com.prodning.turtlesim.combat.data.MicroCombatResult;
 import com.prodning.turtlesim.combat.data.TechLevels;
-import com.prodning.turtlesim.combat.data.MacroCombatResult.ResultType;
 import com.prodning.turtlesim.parse.EntityFileParser;
 
 
@@ -35,7 +35,7 @@ public class Fleet extends ArrayList<CombatEntity> {
 		ArrayList<CombatEntity> attackerLosses = new ArrayList<CombatEntity>();
 		ArrayList<CombatEntity> defenderLosses = new ArrayList<CombatEntity>();
 		
-		final int verbosity = 0;
+		//final int verbosity = 0;
 		
 		//Before the fight, make sure all combat entities have full hull, shields, and correct weapon values
 		for(CombatEntity entity : this) {
@@ -50,7 +50,6 @@ public class Fleet extends ArrayList<CombatEntity> {
 		}
 		
 		int round = 0;
-		Boolean fleetDestroyed = false;
 		
 		//Simulate
 		do {
@@ -65,10 +64,10 @@ public class Fleet extends ArrayList<CombatEntity> {
 			int defenderPower = 0;
 			int defenderShieldTotal = 0;
 			
-			if(verbosity >= 2) {
-				System.out.println("Attacking fleet: " + Fleet.compositionIdToName(this.getFleetComposition()).toString());
-				System.out.println("Defending fleet: " + Fleet.compositionIdToName(defendingFleet.getFleetComposition()).toString());
-			}
+//			if(verbosity >= 2) {
+//				System.out.println("Attacking fleet: " + Fleet.compositionIdToName(this.getFleetComposition()).toString());
+//				System.out.println("Defending fleet: " + Fleet.compositionIdToName(defendingFleet.getFleetComposition()).toString());
+//			}
 			
 			//at beginning of each round, restore shields of all remaining units
 			for(CombatEntity entity : this) {
@@ -152,15 +151,15 @@ public class Fleet extends ArrayList<CombatEntity> {
 			}
 			
 			
-			if (verbosity >= 2) {
-				System.out.println();
-				System.out.println("The attacking fleet fires a total of " + attackerFires + " times with the pwoer of " + attackerPower + " upon the defender.");
-				System.out.println("The defender's shields absorb " + defenderShieldTotal + " damage points");
-				System.out.println();
-				System.out.println("The defending fleet fires a total of " + defenderFires + " times with the pwoer of " + defenderPower + " upon the defender.");
-				System.out.println("The defender's shields absorb " + attackerShieldTotal + " damage points");
-				System.out.println();
-			}
+//			if (verbosity >= 2) {
+//				System.out.println();
+//				System.out.println("The attacking fleet fires a total of " + attackerFires + " times with the pwoer of " + attackerPower + " upon the defender.");
+//				System.out.println("The defender's shields absorb " + defenderShieldTotal + " damage points");
+//				System.out.println();
+//				System.out.println("The defending fleet fires a total of " + defenderFires + " times with the pwoer of " + defenderPower + " upon the defender.");
+//				System.out.println("The defender's shields absorb " + attackerShieldTotal + " damage points");
+//				System.out.println();
+//			}
 			
 		} while ((++round < 6) && (this.size() > 0) && (defendingFleet.size() > 0));
 		
@@ -184,19 +183,19 @@ public class Fleet extends ArrayList<CombatEntity> {
 			mcr.setResultType(ResultType.DRAW);
 		}
 		
-		if (verbosity >= 1) {
-			System.out.println();
-			System.out.println("******** FINAL ********");
-			System.out.println();
-
-			System.out.println("Attacking Fleet:");
-			System.out.println(this.getFleetComposition());
-			System.out.println();
-
-			System.out.println("Defending Fleet:");
-			System.out.println(defendingFleet.getFleetComposition());
-			System.out.println();
-		}
+//		if (verbosity >= 1) {
+//			System.out.println();
+//			System.out.println("******** FINAL ********");
+//			System.out.println();
+//
+//			System.out.println("Attacking Fleet:");
+//			System.out.println(this.getFleetComposition());
+//			System.out.println();
+//
+//			System.out.println("Defending Fleet:");
+//			System.out.println(defendingFleet.getFleetComposition());
+//			System.out.println();
+//		}
 		
 		return mcr;
 	}
@@ -244,6 +243,10 @@ public class Fleet extends ArrayList<CombatEntity> {
 		this.techLevels = techLevels;
 	} public int getNumberOfShips() {
 		return numberOfShips;
+	} public CombatGroup getCombatGroup() {
+		return combatGroup;
+	} public void setCombatGroup(CombatGroup combatGroup) {
+		this.combatGroup = combatGroup;
 	}
 
 	public Fleet deepClone() {
